c3219ae5ac932127adf8f2ef0dbbd10b6cb55e5d
Ewelina Lichocka <ewelina.lichocka70@gmail.com>
2021-02-21 09:12:17 +0100

    Install Sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles/index.css'
+import "../styles/index.css";
+import "../styles/custom/index.sass";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
